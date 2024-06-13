2c88c174fc774d144f4b0234bdb8b04e3c75c10e
MrRocket08 <122652107+MrRocket08@users.noreply.github.com>
2024-06-13 18:51:26 -0400

    added first five pdts onto weapons list!

--- Assets/Scripts/Menus/TelemetryMenu.cs
+++ Assets/Scripts/Menus/TelemetryMenu.cs
@@ -18,21 +18,21 @@ public class TelemetryMenu : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		telemetry.text = "Δv		" + (ship.effectiveExhaustVelocity * Mathf.Log(ship.wetMass / ship.dryMass) / 1000).ToString("F3") + " KM / S" +
-			"\n\nVEL		" + ship.velocity.magnitude.ToString("F2") + " M / S" +
+		telemetry.text = "Δv	" + (ship.effectiveExhaustVelocity * Mathf.Log(ship.wetMass / ship.dryMass) / 1000).ToString("F3") + " KM / S" +
+			"\n\nVEL	" + ship.velocity.magnitude.ToString("F2") + " M / S" +
 			"\n--VELX	" + ship.velocity.x.ToString("F2") + " M / S" +
 			"\n--VELY	" + ship.velocity.y.ToString("F2") + " M / S" +
 
-			"\n\nFUEL		" + ship.fuel.ToString("F1") + " / " + ship.maxFuel.ToString("F1") +
-			"\n--%		" + (ship.fuel / ship.maxFuel * 100).ToString("F2") + " %" +
+			"\n\nFUEL	" + ship.fuel.ToString("F1") + " / " + ship.maxFuel.ToString("F1") +
+			"\n--%	" + (ship.fuel / ship.maxFuel * 100).ToString("F2") + " %" +
 
-			"\n\nKTC		" + ship.ktcAmmo.ToString("F0") + " / " + ship.maxKtcAmmo.ToString("F0") +
-			"\n--%		" + (ship.ktcAmmo / ship.maxKtcAmmo * 100).ToString("F2") + " %" +
+			"\n\nKTC	" + ship.ktcAmmo.ToString("F0") + " / " + ship.maxKtcAmmo.ToString("F0") +
+			"\n--%	" + (ship.ktcAmmo / ship.maxKtcAmmo * 100).ToString("F2") + " %" +
 
-			"\n\nMSL		" + ship.mslAmmo.ToString("F0") + " / " + ship.maxMslAmmo.ToString("F0") +
-			"\n--%		" + (ship.mslAmmo / ship.maxMslAmmo * 100).ToString("F2") + " %" +
+			"\n\nMSL	" + ship.mslAmmo.ToString("F0") + " / " + ship.maxMslAmmo.ToString("F0") +
+			"\n--%	" + (ship.mslAmmo / ship.maxMslAmmo * 100).ToString("F2") + " %" +
 
-			"\n\nHEAT		" + ship.heat.ToString("F1") + " / " + ship.heatCapacity.ToString("F1") + " MW" +
-			"\n--%		" + (ship.heat / ship.heatCapacity * 100).ToString("F2") + " %";
+			"\n\nHEAT	" + ship.heat.ToString("F1") + " / " + ship.heatCapacity.ToString("F1") + " MW" +
+			"\n--%	" + (ship.heat / ship.heatCapacity * 100).ToString("F2") + " %";
 	}
 }
